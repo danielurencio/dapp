2dd7d8a617bf20079a764d0c573864635b8c2b35
Daniel <danielurencio@gmial.com>
2018-03-30 20:19:13 -0600

    d

--- routes.js
+++ routes.js
@@ -5,6 +5,30 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'myproject';
 
 
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
+
+
 router.get('/', function(req, res){
    var data = {
 	title: 'fondín',
@@ -30,7 +54,7 @@ router.get('/confirm',function(req,res) {
 
 router.post('/signup', function(req, res, next) {
     // Use connect method to connect to the server
-    MongoClient.connect(url, function(err, client) {
+    MongoClient.connect(mongoURL, function(err, client) {
         const db = client.db(dbName);
         db.collection("inserts").findOne({ 'email':req.body.email }, function(err,doc) {
 
