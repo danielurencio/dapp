b8330a4af1ad9b382f81243804ff5f68052ee39a
Daniel <danielurencio@gmial.com>
2018-03-30 18:53:46 -0600

    d

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var engines = require('consolidate');
 //var routes = require('./routes.js');
 var bodyParser = require('body-parser');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use(bodyParser.json()); // support json encoded bodies
