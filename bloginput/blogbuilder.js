276d6600fd0fa8aba7bf12f8eddfccc66e6a67c7
danielurencio <danielurencio@gmail.com>
2018-04-15 22:23:00 -0500

    d

--- bloginput/blogbuilder.js
+++ bloginput/blogbuilder.js
@@ -6,39 +6,62 @@ $(document).ready(function() {
 
 function sectionFunctionality() {
   $('div.addSection').on('click',function() {
-    var sections = ['Text','Image','Video','HTML'];
+      var sections = ['Text','Image','Video','HTML'];
+      var height = $(this).css("height");
 
-    var width = String(100 / sections.length) + '%';
-    var height = $(this).css("height");
-    var containerStyle = 'style="height:'+ '100%' +';width:'+ width +
+      var child = sectionGenerator(sections,height);
+
+      if(this.getAttribute('class') == 'addSection') {
+          $(this).text('');
+          $(this).attr('class','chooseContent');
+          $(this).html(child);
+	  //this.querySelector('.container:first-child').id = 'selectedContainer';
+	  $(this).append(sectionGenerator(['Title','Subtitle','Paragraph'],height/2));
+	  $(this).append('<textarea style="resize:none;padding:0px;margin:20px;width:calc(100% - 40px);height:200px;"></textarea>');
+      }
+
+      $('div.container').on('click',function() {
+	  this.parentNode.querySelectorAll('.container').forEach(function(d) {
+	    d.id = '';
+	  });
+	  this.id = 'selectedContainer';
+      });
+
+
+      $('div#undo').on('click',function() {
+          $(this.parentNode.parentNode.parentNode)
+	      .html('<div class="addSection">&oplus;</div>');
+
+          sectionFunctionality();
+      });
+
+  });
+};
+
+
+function sectionGenerator(sections,height) {
+
+      var width = String(100 / sections.length) + '%';
+      var containerStyle = 'style="height:'+ '100%' +';width:'+ width +
 							';display:table-cell;"';
 
-    var sectionContainer = sections.map(function(d) {
-	return '<div '+ containerStyle +' class="container">'+ d +'</div>'
-    }).join('');
+      var sectionContainer = sections.map(function(d,i) {
+	  var id = i == 0 ? 'id="selectedContainer" ' : '';
+	  var str_ = '<div '+ id + containerStyle +' class="container">'+ d +'</div>';
+	  return str_;
+      }).join('');
 
-    var child = '<div style="display:table;width:100%;height:'+ height +'">' +
-		  '<div style="height:100%;display:table-cell;width:calc(100% - '
+      var child = '<div style="display:table;width:100%;height:'+ height +'">' +
+		    '<div style="height:100%;display:table-cell;width:calc(100% - '
 								      + height + ')">'+
 			'<div style="display:table;width:100%;height:100%;">' +
 				sectionContainer +
 			'</div>' +
-		  '</div>' +
-		  '<div id="undo" style="background:red;display:table-cell;width:'
+		    '</div>' +
+		    '<div id="undo" style="background:red;display:table-cell;width:'
 									+ height +'">'+
-		  '</div>' +
-	        '</div>';
+		    '</div>' +
+	          '</div>';
 
-    $(this).text('');
-    $(this).attr('class','chooseContent');
-    $(this).html(child);
-
-    $('div#undo').on('click',function() {
-      this.parentNode.parentNode.parentNode
-	      .innerHTML = '<div class="addSection">&oplus;</div>';
-
-      sectionFunctionality();
-    });
-
-  });
+      return child;
 }
